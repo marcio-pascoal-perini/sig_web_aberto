d41516176ee824f43863b4304104947a78db3358
Márcio Pascoal Perini <mpperini@gmail.com>
2022-01-14 11:09:38 -0300

    2022-01-14 11:10:00

--- PIMPF1.R
+++ PIMPF1.R
@@ -55,8 +55,13 @@ PIMPF1_Admin <- function(session) {
   
   ## variáveis e vetores ##
   
-  mes <- 202106
-  meses <- c('Junho 2021' = 202106,
+  mes <- 202111
+  meses <- c('Novembro 2021' = 202111,
+             'Outubro 2021' = 202110,
+             'Setembro 2021' = 202109,
+             'Agosto 2021' = 202108,
+             'Julho 2021' = 202107,
+             'Junho 2021' = 202106,
              'Maio 2021' = 202105,
              'Abril 2021' = 202104,
              'Março 2021' = 202103,
@@ -73,19 +78,7 @@ PIMPF1_Admin <- function(session) {
              'Abril 2020' = 202004,
              'Março 2020' = 202003,
              'Fevereiro 2020' = 202002,
-             'Janeiro 2020' = 202001,
-             'Dezembro 2019' = 201912,
-             'Novembro 2019' = 201911,
-             'Outubro 2019' = 201910,
-             'Setembro 2019' = 201909,
-             'Agosto 2019' = 201908,
-             'Julho 2019' = 201907,
-             'Junho 2019' = 201906,
-             'Maio 2019' = 201905,
-             'Abril 2019' = 201904,
-             'Março 2019' = 201903,
-             'Fevereiro 2019' = 201902,
-             'Janeiro 2019' = 201901
+             'Janeiro 2020' = 202001
   )
   
   atividade <- 129314
